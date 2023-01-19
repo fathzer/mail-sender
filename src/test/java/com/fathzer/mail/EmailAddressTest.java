90a544cc664fbc0b3f1eca96d1bae65a29d7ee47
Fathzer <jm-dev@astesana.net>
2023-01-19 19:49:16 +0100

    More tests

--- src/test/java/com/fathzer/mail/EmailAddressTest.java
+++ src/test/java/com/fathzer/mail/EmailAddressTest.java
@@ -11,9 +11,18 @@ class EmailAddressTest {
 	@Test
 	void test() {
 		assertThrows(IllegalArgumentException.class, () -> new EMailAddress(null));
+		assertThrows(IllegalArgumentException.class, () -> new EMailAddress("no a valid one"));
+
 		final List<EMailAddress> lst = EMailAddress.list("a@b.com","c@d.net");
 		assertEquals(2, lst.size());
 		assertTrue(lst.contains(new EMailAddress("a@b.com")));
 		assertTrue(lst.contains(new EMailAddress("c@d.net")));
+		
+		final EMailAddress addr = new EMailAddress("a@b.com");
+		assertEquals(addr.hashCode(), new EMailAddress("a@b.com").hashCode());
+		
+		assertNotEquals(null, addr);
+		assertNotEquals("a@b.com", addr);
+		assertEquals (addr,addr);
 	}
 }
--- src/test/java/com/fathzer/mail/EmailTest.java
+++ src/test/java/com/fathzer/mail/EmailTest.java
@@ -16,6 +16,21 @@ class EmailTest {
 		assertEquals(EMailAddress.list("a@b.com"),mail.getRecipients());
 		assertEquals("test",mail.getSubject());
 		assertEquals("content",mail.getContent());
+		assertEquals(MimeType.TEXT, mail.getMimeType());
+		assertNull(mail.getSender());
+		assertNull(mail.getReplyTo());
+		
+		assertThrows(IllegalArgumentException.class, () -> mail.withMimeType(null));
+		mail.withMimeType(MimeType.HTML);
+		assertEquals(MimeType.HTML, mail.getMimeType());
+		
+		mail.withSender(new EMailAddress("me@gmail.com"));
+		assertEquals(new EMailAddress("me@gmail.com"), mail.getSender());
+		
+		mail.withReplyTo(EMailAddress.list("x@y.net","z@0.fr"));
+		assertEquals(2, mail.getReplyTo().size());
+		assertTrue(mail.getReplyTo().contains(new EMailAddress("x@y.net")));
+		assertTrue(mail.getReplyTo().contains(new EMailAddress("z@0.fr")));
 	}
 
 }
