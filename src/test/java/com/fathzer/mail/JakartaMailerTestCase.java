5a5742319a2bdb51dad286b2f84121049847f291
Fathzer <jm-dev@astesana.net>
2023-01-20 17:22:12 +0100

    Adds more tests

--- src/test/java/com/fathzer/mail/JakartaMailerTestCase.java
+++ src/test/java/com/fathzer/mail/JakartaMailerTestCase.java
@@ -2,12 +2,15 @@ package com.fathzer.mail;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 
 import jakarta.mail.Address;
 import jakarta.mail.Message;
+import jakarta.mail.Message.RecipientType;
+import jakarta.mail.MessagingException;
 import jakarta.mail.Transport;
 
 class JakartaMailerTestCase {
@@ -20,10 +23,11 @@ class JakartaMailerTestCase {
 		mailer.setDebug(true);
 		assertTrue(mailer.getSession().getDebug());
 		
-		Recipients to = Recipients.to("me@yahoo.de");
+		Recipients to = Recipients.to("to@yahoo.de");
 		EMail mail = new EMail(to, "subject", "content");
 		mailer.send(mail);
 		assertArrayEquals(to.getTo().stream().map(EMailAddress::getAddress).toArray(Address[]::new), Transport.lastMsg.getRecipients(Message.RecipientType.TO));
+		testRecipientsAre(RecipientType.TO, new String[]{"to@yahoo.de"});
 		assertEquals(1, Transport.lastMsg.getFrom().length);
 		assertEquals(defaultSender.getAddress(), Transport.lastMsg.getFrom()[0]);
 		assertEquals("subject", Transport.lastMsg.getSubject());
@@ -32,11 +36,22 @@ class JakartaMailerTestCase {
 		
 		EMailAddress other = new EMailAddress("other@amazon.com");
 		mail.withMimeType(MimeType.HTML).withSender(other).withReplyTo(Collections.singletonList(defaultSender));
+		to.setCc(EMailAddress.list("cc@gmail.com"));
+		to.setBcc(EMailAddress.list("bcc@gmail.com"));
 		mailer.send(mail);
 		assertEquals(1, Transport.lastMsg.getFrom().length);
 		assertEquals(other.getAddress(), Transport.lastMsg.getFrom()[0]);
 		assertEquals("text/html", Transport.lastMsg.getDataHandler().getContentType());
 		assertEquals(1, Transport.lastMsg.getReplyTo().length);
 		assertEquals(defaultSender.getAddress(), Transport.lastMsg.getReplyTo()[0]);
+		testRecipientsAre(RecipientType.TO, new String[]{"to@yahoo.de"});
+		testRecipientsAre(RecipientType.CC, new String[]{"cc@gmail.com"});
+		testRecipientsAre(RecipientType.BCC, new String[]{"bcc@gmail.com"});
+//		assertEquals(Transport.lastMsg.getRecipients(Recipients))
+	}
+	
+	void testRecipientsAre(RecipientType type, String[] mails) throws MessagingException {
+		assertArrayEquals(mails, Arrays.stream(Transport.lastMsg.getRecipients(type)).map(Object::toString).toArray());
+
 	}
 }
